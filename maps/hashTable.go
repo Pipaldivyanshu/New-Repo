b82c27b55c447fb815427d9739abd239c01dc256
Divyanshu Pipal <divyanshupipal@Divyanshus-MacBook-Air.local>
2019-08-13 19:56:26 -0500

    Change

--- maps/hashTable.go
+++ maps/hashTable.go
@@ -5,8 +5,9 @@ import (
 )
 
 func main() {
-	var country map[int]string
-	country = make(map[int]string)
+	//var country map[int]string
+
+	country := make(map[int]string)
 	country[1] = "India"
 	country[2] = "China"
 	country[3] = "Pakistan"
@@ -15,5 +16,6 @@ func main() {
 	country[6] = "Indonesia"
 	for i, j := range country {
 		fmt.Printf("Key: %d Value: %s\n", i, j)
+
 	}
 }
